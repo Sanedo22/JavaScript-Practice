3f0f3e320dc40b5bdf9b064bd439cea9a784f104
Dhruv Patni <patnidhruv13@gmail.com>
2026-04-07 12:32:27 +0530

    Arrays end

--- arrays.js
+++ arrays.js
@@ -3,3 +3,57 @@
 // for (let i = 0; i < cars.length; i++){
 //     console.log(cars[i]);
 // }
+
+// for of
+// for (let car of cars){
+//     console.log(car);
+// }
+
+// for in
+// for (let car in cars){
+//     console.log(cars[car]);
+// }
+
+// for (let car of cars){
+//     console.log(car.toLowerCase()); //we can also use methods on the elements of the array
+// }
+
+// avg of numbers in an array
+// let nums = [10, 22, 34, 46, 57];
+
+// let sum = 0;
+
+// for (let num of nums){
+//     sum += num;
+// }
+
+// let avg = sum / nums.length;
+// console.log(`The average of the numbers is: ${avg}`);
+
+// value of offer discount
+// let price = [1001, 2003, 3004, 4005, 5006];
+
+// let discount = 15;
+
+// for (let i = 0; i < price.length; i++){
+//     let dprice = price[i] / discount;
+//     price[i] -= dprice;
+//     console.log(`The discounted price of item ${i+1} is: ${price[i]}`);
+// }
+
+// array methods
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+// fruits.push("Fig"); // adds an element to the end of the array
+// fruits.pop(); // removes the last element of the array
+// console.log(fruits.toString()); // converts the array to a string
+
+//concate arrays
+// let vegetables = ["Carrot", "Broccoli", "Spinach"];
+// let food = fruits.concat(vegetables);
+// console.log(food);
+
+// fruits.shift();
+// fruits.splice(2, 1, "Mango");
+// fruits.pop();
+// fruits.push("gauva");
+// console.log(fruits);

1b1547ea0f53b38e1ef18ef036fdf57275b14453
Dhruv Patni <patnidhruv13@gmail.com>
2026-04-22 15:45:38 +0530

    function methods and object methods

--- Practice/conditions.js
+++ Practice/conditions.js
@@ -31,18 +31,122 @@ let day = "sat";
 //     console.log("Weekend");
 // }
 
-switch(day) {
-    case "mon": 
-        console.log("7am");
-        break;
-    case "tue":
-    case "wed":
-    case "thu":
-        console.log("6am");
-        break;
-    case "fri":
-        console.log("9am");
-        break;
-    default:
-        console.log("Weekend");
+// switch(day) {
+//     case "mon": 
+//         console.log("7am");
+//         break;
+//     case "tue":
+//     case "wed":
+//     case "thu":
+//         console.log("6am");
+//         break;
+//     case "fri":
+//         console.log("9am");
+//         break;
+//     default:
+//         console.log(`today
+//             is
+//             weekend`);
+// }
+
+// let i = 1;
+// let temp = 1;
+
+// while(temp<=5) {
+//     console.log(i);
+//     console.log(temp);
+    
+//     temp++;
+// }
+
+// let a = "*";
+
+// //print a star pattern
+// for(let i=1; i<=10; i++) {
+//     let str = "";
+//     for(let j=1; j<=i; j++) {
+//         str += a;
+//     }
+//     console.log(str);
+// }
+
+// let i = 1;
+
+// for(i = 1;i<=10; i++) {
+//     console.log("Hi", i);
+//     i++;
+//     i++;
+    
+// }
+
+// let a = "*";
+
+// for(let i = 1; i<=5; i++){
+//     let b = "";
+//     for(let j = 1; j<=i; j++){
+//         b +=a;
+//     }
+//     console.log(b);
+// }
+
+//print a triangle pattern, starting from center
+// let a = 5;
+
+// for(let i = 1; i<=5; i++){
+//     let b = "";
+//     for(let j = 1; j<=i; j++){
+//         b +=a;
+//     }
+//     console.log(b);
+// }
+
+// function user(name, age) {
+//     return `Hello ${name}, you are ${age} years old`;
+// }
+
+// let name = "Dhruv";
+// let str = user(name);
+// console.log(str);
+
+// let add = function(num1, num2) {
+//     return num1 + num2;
+// }
+
+// let sum = add(5);
+// console.log(sum);
+
+//add function with negative numbers getting add
+
+// function add(num1, num2) {
+//     if(num1 < 0 || num2 < 0) {
+//         return "Negative numbers are not allowed";
+//     }
+//     return num1 + num2;
+// }
+
+// let sum = (num1, num2) => Math.abs(num1) + Math.abs(num2);
+
+// console.log(sum(-5, -10));
+
+let laptop1 = {
+    cpu: "i7",
+    ram: "16gb",
+    brand: "Dell",
+
+    getConfig: function() {
+        console.log(this.cpu);
+        
+    }
 }
+
+let laptop2 = {
+    cpu: "i5",
+    ram: "8gb",
+    brand: "HP",
+
+    getConfig: function() {
+        console.log(this.cpu);
+    }
+}
+
+laptop2.getConfig();

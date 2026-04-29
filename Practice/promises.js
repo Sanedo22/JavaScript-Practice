1fecb217d63f8c6e5f7aabcc017b5bf105823da4
Dhruv Patni <patnidhruv13@gmail.com>
2026-04-29 16:04:49 +0530

    forth commit for today

---
+++ Practice/promises.js
@@ -0,0 +1,12 @@
+let cart = ["shoes", "pants", "kurtas"];
+
+const promise = createOrder(cart);
+
+function createOrder(cart) {
+
+    const pr = new Promise(function(resolve, reject){
+
+    });
+
+    return pr;
+}
